2ef7cf48f87147fd6df094e5eb9df644a6238b41
Jace Callihoo <jacecallihoo@gmail.com>
2024-03-17 21:13:34 -0600

    -

--- scripts/main.sh
+++ scripts/main.sh
@@ -1,11 +1,13 @@
 # set -ex
 
 os_id=$(source /etc/os-release; echo $ID)
-requirements_path=$JACE_ENVIRONMENT/scripts/ansible/requirements
+requirements_path=$JACE_ENV/scripts/ansible/requirements
 
 echo "\$ID: $ID"
 echo "\$os_id: $os_id"
 
 ansible-galaxy install -r $requirements_path/common.yml
 ansible-galaxy install -r $requirements_path/$os_id.yml
+
+ansible-playbook --ask-become-pass $JACE_ENV/scripts/ansible/main.yml
 
