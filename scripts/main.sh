7d85e41f5679ba5f72df07fc82fe7503613c2a79
Jace Callihoo <jacecallihoo@gmail.com>
2024-03-19 22:18:42 -0600

    -

--- scripts/main.sh
+++ scripts/main.sh
@@ -3,9 +3,6 @@
 os_id=$(source /etc/os-release; echo $ID)
 requirements_path=$JACE_ENV/scripts/ansible/requirements
 
-echo "\$ID: $ID"
-echo "\$os_id: $os_id"
-
 ansible-galaxy install -r $requirements_path/common.yml
 ansible-galaxy install -r $requirements_path/$os_id.yml
 
